f95cc70c960defc6dc2e0007140bdaaaa77bc15a
NoRuTnT <moonabcd@naver.com>
2025-02-19 03:16:06 +0900

    init: initial backend-project setting

---
+++ backend/src/test/java/com/raidersleader/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raidersleader.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
