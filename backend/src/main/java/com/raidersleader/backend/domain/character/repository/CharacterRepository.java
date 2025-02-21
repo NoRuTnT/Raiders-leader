8747dbd34c0d9c751ab7e8031496b513f7141259
NoRuTnT <moonabcd@naver.com>
2025-02-22 02:55:28 +0900

    fix : sql문 명시, Adventure검색결과 List로 수정

--- backend/src/main/java/com/raidersleader/backend/domain/character/repository/CharacterRepository.java
+++ backend/src/main/java/com/raidersleader/backend/domain/character/repository/CharacterRepository.java
@@ -1,15 +1,23 @@
 package com.raidersleader.backend.domain.character.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.raidersleader.backend.domain.character.model.dto.CharacterDetailResponseDto;
 import com.raidersleader.backend.domain.character.model.entity.Character;
 
 public interface CharacterRepository extends JpaRepository<Character,Long> {
 
-	Optional<Character> findByAdventureName(String adventureName);
+	@Query("SELECT c FROM Character c WHERE c.adventureName = ?1")
+	List<Character> findByAdventureName(String adventureName);
+
+	@Query("SELECT c FROM Character c WHERE c.characterName = ?1")
 	Optional<Character> findByCharacterName(String characterName);
+
+	@Query("SELECT c FROM Character c WHERE c.characterId = ?1")
 	Optional<Character> findByCharacterId(String characterId);
 
 
